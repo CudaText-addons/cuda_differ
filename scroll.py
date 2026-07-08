35ab5dcc593e2d49999680eddb1ce6d2c518ffdc
Alexey T <support@uvviewsoft.com>
2026-07-08 17:34:51 +0300

    avoid depre API prop_scroll_horz_smooth

--- scroll.py
+++ scroll.py
@@ -21,8 +21,8 @@ class ScrollSplittedTab:
         if ed_self.get_prop(ct.PROP_SPLIT)[0] == '-':
             return
 
-        pos_v = ed_self.get_prop(ct.PROP_SCROLL_VERT_SMOOTH)
-        pos_h = ed_self.get_prop(ct.PROP_SCROLL_HORZ_SMOOTH)
+        pos_v = ed_self.get_prop(ct.PROP_SCROLL_VERT_INFO)['smooth_pos']
+        pos_h = ed_self.get_prop(ct.PROP_SCROLL_HORZ_INFO)['smooth_pos']
 
         hndl_self = ed_self.get_prop(ct.PROP_HANDLE_SELF)
         hndl_primary = ed_self.get_prop(ct.PROP_HANDLE_PRIMARY)
@@ -33,7 +33,7 @@ class ScrollSplittedTab:
             hndl_opposit = hndl_primary
         e = ct.Editor(hndl_opposit)
 
-        e.set_prop(ct.PROP_SCROLL_VERT_SMOOTH, pos_v)
-        e.set_prop(ct.PROP_SCROLL_HORZ_SMOOTH, pos_h)
+        e.set_prop(ct.PROP_SCROLL_VERT_INFO, {'smooth_pos': pos_v})
+        e.set_prop(ct.PROP_SCROLL_HORZ_INFO, {'smooth_pos': pos_h})
 
         e.cmd(ct_cmd.cmd_RepaintEditor)
